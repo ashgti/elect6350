dfc5f16f1154eeb1bf235f392b626f0c1197bef7
William Woodall <wjwwood@gmail.com>
2011-10-12 17:24:10 -0500

    Adding working on matplotlib support

--- hw4/costmapwidget.py
+++ hw4/costmapwidget.py
@@ -5,17 +5,12 @@ from Queue import Queue
 
 from PySide import QtCore, QtGui
 
+import matplotlib
+matplotlib.use('AGG')
+from matplotlib.pylab import gcf, imshow, draw
+
 from costmap import Costmap2D
 from obstacle import Obstacle
-from brushfire import BrushfireExpansion
-
-class CellWidget(QtGui.QLabel):
-    """Represents a Cell Widget"""
-    def __init__(self, value = 0.0, parent = None):
-        QtGui.QLabel.__init__(self, parent=parent)
-        self.value = value
-        self.text = str(self.value)
-    
 
 class Costmap2DWidget(QtGui.QWidget):
     costmap_changed = QtCore.Signal()
@@ -25,25 +20,38 @@ class Costmap2DWidget(QtGui.QWidget):
         QtGui.QWidget.__init__(self, parent)
         self.costmap = costmap
         
-        self.create_cell_grid()
+        # Setup display
+        self.imshow = imshow(self.costmap.data.T, interpolation='nearest')
+        self.figure = gcf()
+        self.image_label = QtGui.QLabel()
+        self.image_label.setBackgroundRole(QtGui.QPalette.Base)
         
-        self.change_queue = Queue()
+        # Setup layout
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.image_label)
+        self.setLayout(layout)
+        
+        # Idle control
         self.idle = True
         
+        # Make Qt Connections
         self.connect_stuff()
         
+        # Override costmap on change
         self.costmap.on_update = self.costmap_update_callback
     
-    def create_cell_grid(self):
-        """Creates the grid of cells"""
-        self.grid_cells = [[None]*self.costmap.height]*self.costmap.width
-        self.grid_cells_layout = QtGui.QGridLayout()
-        for (x, row) in enumerate(self.costmap):
-            for (y, cell_value) in enumerate(row):
-                cell = CellWidget(cell_value)
-                self.grid_cells[x][y] = cell
-                self.grid_cells_layout.addWidget(cell, y, x)
-        self.setLayout(self.grid_cells_layout)
+    def paintEvent(self, vent):
+        """Overrides the default paint event"""
+        self.figure = gcf()
+        # Render the canvas
+        self.figure.canvas.draw()
+        # Convert the render into a string buffer
+        str_buffer = self.figure.canvas.buffer_rgba(0,0)
+        # Get the bounding box of the render
+        l,b,w,h = self.figure.bbox.bounds
+        # Create and set the Pixmap from the render
+        image = QtGui.QImage(str_buffer, w, h, QtGui.QImage.Format_ARGB32)
+        self.image_label.setPixmap(QtGui.QPixmap.fromImage(image))
     
     def connect_stuff(self):
         """Make Qt connections"""
@@ -51,25 +59,41 @@ class Costmap2DWidget(QtGui.QWidget):
     
     def costmap_update_callback(self, key, val):
         """Callback to handle when the map is updated"""
-        self.change_queue.put((key, val))
         if self.idle:
             self.idle = False
             self.costmap_changed.emit()
     
     def on_map_update(self):
         """Slot to handle the costmap_changed signal"""
-        while not self.change_queue.empty():
-            key, val = self.change_queue.get()
-            # Do Work
-            if slice in [type(key[0]), type(key[1])]:
-                print key, val
-            self.change_queue.task_done()
+        self.imshow.set_array(self.costmap.data.T)
+        print type(self.imshow)
+        self.repaint()
+        print('Should be repainting')
         self.idle = True
     
 
+def _run_brushfire(c):
+    """Runs the brushfire"""
+    from brushfire import BrushfireExpansion
+    be = BrushfireExpansion(c)
+    be.set_ignition_cells([(0,0)])
+    import time
+    while be.step_solution():
+        pass
+        # time.sleep(0.1)
+
+def _run_voronoi_expansion(c):
+    """Runs the voronoi expansion"""
+    from voronoi import VoronoiExpansion
+    ve = VoronoiExpansion(c)
+    import time
+    while ve.step_solution():
+        pass
+        time.sleep(0.1)
+
 if __name__ == '__main__':
     try:
-        c = Costmap2D(10,20,resolution=0.5)
+        c = Costmap2D(10,20,resolution=0.1)
         Obstacle(3,3,3,3).draw(c)
         Obstacle(5,9,3,3).draw(c)
         Obstacle(4,16,3,3).draw(c)
@@ -77,15 +101,9 @@ if __name__ == '__main__':
         app = QtGui.QApplication(sys.argv)
         cw = Costmap2DWidget(c)
         
-        be = BrushfireExpansion(c)
-        be.set_ignition_cells([(0,0)])
-        be.solve()
-        
-        import numpy as np
-        c[1,1:5] = np.array([-7,-7,-7,-7])
-        c[2:6,1] = np.array([-9,-9,-9,-9])
-        
-        print c
+        import threading
+        threading.Thread(target=_run_brushfire, args=(c,)).start()
+        # threading.Thread(target=_run_voronoi_expansion, args=(c,)).start()
         
         cw.show()
         sys.exit(app.exec_())
